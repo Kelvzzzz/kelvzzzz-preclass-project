abac08d0a37d34c91e7a08494b211eb070e1710c
kelvzzzz <kelvinmatthew28@example.com>
2022-06-10 16:42:41 +0800

    activity_2.rb:Hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,17 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def find_name(age_value)
+
+  names = { firstname: 'kelvzzz', middlename: 'austria', lastname: 'abad' }
+
+
+  puts "#{names[:firstname]}"
+  puts "#{names[:middlename]}"
+  puts "#{names[:lastname]}"
+
+
+end
+
+p find_name(24)
